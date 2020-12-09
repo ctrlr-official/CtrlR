6d4559115188d606bf9a7833455d1df809082df1
ctrlr-official <74377059+ctrlr-official@users.noreply.github.com>
2020-12-09 21:39:05 +0400

    Update Quizik.js

--- src/Components/Quiz/Quizik.js
+++ src/Components/Quiz/Quizik.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './Quizik.css';
-import QuizMain from './NewProject/QuizMain'
+import QuizMain from './NewProject/QuizMain';
+import { Link } from 'react-router-dom';
 const Quizik = () => {
     return (
 <div className='Quizik'>
+//         <div className="paragraphik"> <p> Here you can take a litlle quiz about cybercrime</p></div>
+//         <div class="linkik">
+         
+//         </div>
     <QuizMain />
         </div>
     )
