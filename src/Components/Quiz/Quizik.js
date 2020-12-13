56910ff8ce71f2e1eb7a70e14f87a36afd9cf705
ctrlr-official <74377059+ctrlr-official@users.noreply.github.com>
2020-12-13 20:58:21 +0400

    Update Quizik.js

--- src/Components/Quiz/Quizik.js
+++ src/Components/Quiz/Quizik.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Quizik.css';
 import QuizMain from './NewProject/QuizMain';
+import NewsFeed from './NewsFeed/NewsFeed';
 import { Link } from 'react-router-dom';
 const Quizik = () => {
    
