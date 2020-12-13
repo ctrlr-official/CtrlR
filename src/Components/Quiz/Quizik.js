f90e5d466872e09cf462a5b654f4f7d7945ef3c5
ctrlr-official <74377059+ctrlr-official@users.noreply.github.com>
2020-12-13 20:47:07 +0400

    Update Quizik.js

--- src/Components/Quiz/Quizik.js
+++ src/Components/Quiz/Quizik.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Quizik.css';
 import QuizMain from './NewProject/QuizMain';
 import { Link } from 'react-router-dom';
 const Quizik = () => {
+    const [check, setCheck] = useState(false)
     return (
 <div className='Quizik'>
-//         <div className="paragraphik"> <p> Here you can take a litlle quiz about cybercrime</p></div>
-//         <div class="linkik">
-         
-//         </div>
-    <QuizMain />
+        
+<div
+        onClick={() => setCheck(!check)}
+        className={check ? 'transformed' : 'QuizMain'}
+      />
         </div>
     )
 }
